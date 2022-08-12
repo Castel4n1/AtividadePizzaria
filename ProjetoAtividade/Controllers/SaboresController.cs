101a80d9d148f93cdeeca86f367159f988b27f6b
KAUE CASTELANI DO BOMFIM <kaue.cbomfim@senacsp.edu.br>
2022-08-11 22:10:43 -0300

    Criação das controllers

---
+++ ProjetoAtividade/Controllers/SaboresController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAtividade.Controllers
+{
+    public class SaboresController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+
+    }
+}
